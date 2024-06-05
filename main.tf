f44aa50c96f654a67143601a41fe713faea97318
viks1102 <viks_1102@yahoo.com>
2024-06-05 18:46:44 +0100

    wip

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-# Enable Cloud SQL Admin API
-resource "google_project_service" "sqladmin" {
-  service = "sqladmin.googleapis.com"
-  project = var.project_id
-}
+# # Enable Cloud SQL Admin API
+# resource "google_project_service" "sqladmin" {
+#   service = "sqladmin.googleapis.com"
+#   project = var.project_id
+# }
 
-resource "google_sql_database" "database" {
-  name     = "my-database"
-  instance = google_sql_database_instance.mysql.name
-}
+# resource "google_sql_database" "database" {
+#   name     = "my-database"
+#   instance = google_sql_database_instance.mysql.name
+# }
 
 
-resource "google_sql_database_instance" "mysql" {
-  name             = var.db_instance_name
-  database_version = var.db_version
-  region           = var.region
-  project          = var.project_id
+# resource "google_sql_database_instance" "mysql" {
+#   name             = var.db_instance_name
+#   database_version = var.db_version
+#   region           = var.region
+#   project          = var.project_id
 
-  settings {
-    tier = "db-f1-micro"
-  }
-  deletion_protection  = "false"
-}
+#   settings {
+#     tier = "db-f1-micro"
+#   }
+#   deletion_protection  = "false"
+# }
 
-resource "google_sql_user" "mysql" {
-  name     = var.db_user
-  instance = google_sql_database_instance.mysql.name
-  password = var.db_password
-}
+# resource "google_sql_user" "mysql" {
+#   name     = var.db_user
+#   instance = google_sql_database_instance.mysql.name
+#   password = var.db_password
+# }
